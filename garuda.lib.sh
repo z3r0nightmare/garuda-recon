8c885b2a4e817e007f58524160c8143b4a1f8e55
z3r0nightmare <z3r0.nightmare.main@gmail.com>
2024-09-19 06:15:42 +0100

    finalised the subdomain enumeration function and it works well

--- garuda.lib.sh
+++ garuda.lib.sh
@@ -4,7 +4,7 @@ greeting(){
  echo "hey this is garuda scipt for automating your bug bounty experience and scanning.What do you wish to accomplish?"
 }
 options(){
-    echo p "1.Quick scan for a domain \n 2.Quick scan for a list of domains \n 3.Manual scanning with options \n 4.Only single utility use"
+    echo -e "1.Quick scan for a domain \n2.Quick scan for a list of domains \n3.Manual scanning with options \n4.Only single utility use"
     
 }
 
@@ -25,23 +25,24 @@ wildcard_filtering(){
 }
 subdomain_enumeration(){
     subfinder -dL ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_wildcard_scope -o ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_subfinder_output
-    first_run=1
+    firstrun=1
 
     for line in $(cat ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_wildcard_scope); do
      sublist3r -d $line -o ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_temp
 
-        if [$first_run == 1]
-            #cat ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_temp > ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_subdomains
+     if [[ $firstrun == 1 ]]
+        then     
+            cat ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_temp > ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_sublist3r_output
         else 
-            #cat ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_temp >> ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_subdomains
+            cat ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_temp >> ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_sublist3r_output
         fi
-     first_run=2
+     firstrun=2
 
     done
 
-    cat ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_subfinder_output > ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_subdomains
-    cat ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_sublist3r_output >> ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_subdomains
-    cat ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_subdomains | sort | uniq > ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_subdomains
+    cat ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_subfinder_output > ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_temp
+    cat ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_sublist3r_output >> ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_temp
+    cat ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_temp | sort | uniq > ~/bug-bounty/programs/${DOMAIN}/${DOMAIN}_scope/${DOMAIN}_subdomains
 
 
 }
